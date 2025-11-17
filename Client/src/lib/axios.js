5234c497f338e7ce4426fb368fcda0587110fa3a
piyushkhatri968 <piyushkhatri968@gmail.com>
2025-11-17 17:38:13 +0500

    .

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://emedshub-server.vercel.app/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  // baseURL: "https://emedshub-server.vercel.app/api",
   // baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
